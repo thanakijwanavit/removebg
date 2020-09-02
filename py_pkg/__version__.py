8398d715247cbfc3fbe5744569c5caef717c3ed3
nic <nwanavit@hatari.cc>
2020-09-02 11:21:26 +0700

    initial commit

---
+++ py_pkg/__version__.py
@@ -0,0 +1,14 @@
+"""
+__version__.py
+~~~~~~~~~~~~~~
+
+Information about the current version of the py-package-template package.
+"""
+
+__title__ = 'villa-encryptor-thanakijwanavit'
+__description__ = 'encrypt and decrypt data based on villa api'
+__version__ = '0.0.1'
+__author__ = 'Nic Wanavit'
+__author_email__ = 'nwanavit@hatari.cc'
+__license__ = 'Apache 2.0'
+__url__ = 'https://github.com/thanakijwanavit/villaWallet'
